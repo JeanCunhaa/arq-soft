ebd531e9b29073bd73f2781b1ff6e1f779cf2261
Jean Cunha <106135357+JeanCunhaa@users.noreply.github.com>
2023-06-22 20:28:40 -0300

    Delete backend/src/main/java/com/wafflecorp/store directory

--- backend/src/main/java/com/wafflecorp/store/model/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.wafflecorp.store.model;
-
-public enum OrderStatus {
-    CANCELED,
-    PENDING,
-    ORDERED,
-    SHIPPED,
-    DELIVERED
-}
--- backend/src/main/java/com/wafflecorp/store/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wafflecorp.store.repository;
-
-import com.wafflecorp.store.model.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product,Integer> {
-
-}
